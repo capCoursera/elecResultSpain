1608a465e4e6adb263b06d9890dacd2fa9f33585
CAP@Voyager <CAP@Voyager>
2015-09-15 22:55:05 +0200

    - Añadido código para calcular resultados (votos) por mesa.

--- auxCode.R
+++ auxCode.R
@@ -1,7 +1,48 @@
-v6 <- read06file("data/06041105.DAT")
-v5 <- read05file("data/05041105.DAT")
-v4 <- read04file("data/04041105.DAT")
-v3 <- read03file("data/03041105.DAT")
+source('~/devel/elecResultSpain/lib/ProcessFiles.R')
+
+#1.- Fichero de CONTROL de los ficheros que componen el proceso electoral.
+v1 <- read01file("data/01041105.DAT")
+
+#2.- Fichero de IDENTIFICACION del proceso electoral.
 v2 <- read02file("data/02041105.DAT")
-v1 <- read01file(filename = "data/01041105.DAT")
+
+#3.- Fichero de CANDIDATURAS.
+v3 <- read03file("data/03041105.DAT")
+
+#4.- Fichero de RELACION DE CANDIDATOS.
+v4 <- read04file("data/04041105.DAT")
+
+#5.- Fichero de DATOS COMUNES DE MUNICIPIOS.
+v5 <- rbind(read05file("data/05041105.DAT"),read05file("data/05101105.DAT"))
+
+#6.- Fichero de DATOS DE CANDIDATURAS DE MUNICIPIOS
+v6 <- rbind(read06file("data/06041105.DAT"),read06file("data/06101105.DAT"))
+
+#7.- Fichero de DATOS COMUNES DE AMBITO SUPERIOR AL MUNICIPIO.
+v7 <- rbind(read07file("data/07041105.DAT"),read07file("data/07101105.DAT"))
+
+#8.- Fichero de DATOS DE CANDIDATURAS DE AMBITO SUPERIOR AL MUNICIPIO.
+v8 <- rbind(read08file("data/08041105.DAT"),read08file("data/08101105.DAT"))
+
+#9.- Fichero de DATOS COMUNES DE MESAS y del C.E.R.A.
+v9 <- read09file("data/09041105.DAT")
+
+#10.- Fichero de DATOS DE CANDIDATURAS DE MESAS y del C.E.R.A.
+v10 <- read10file("data/10041105.DAT")
+
+#1104.- Fichero de DATOS COMUNES DE MUNICIPIOS menores de 250 habitantes. (Solo en Elecciones Municipales)
+v1104 <- read1104file("data/11041105.DAT")
+
+#1204.- Fichero de DATOS DE CANDIDATURAS DE MUNICIPIOS menores de 250 hab. (Solo en Elecciones Municipales)
+v1204 <- read1204file("data/12041105.DAT")
+
 merge(v3,v6[(v6$CodProv == 28) & (v6$CodMun ==79) & (v6$MunDistrict == 99),]) -> resMad
+merge(v6[(v6$CodProv == 28) & (v6$MunDistrict == 99),],v3) -> resComMad
+resMad[,c(1:3,5:8,11,9)]->resMadVotos
+
+dcast(data= resMadVotos, formula = ElType+ElYear+ElMonth+Round+CodProv+CodMun+MunDistrict~ candAcronym, value.var="CandVotos", fill=0) -> rowMadVotes
+
+resMad[,c(1:3,5:8,11,10)]->resMadEsc
+dcast(data= resMadEsc, formula = ElType+ElYear+ElMonth+Round+CodProv+CodMun+MunDistrict~ candAcronym, value.var="CandObt", fill=0) -> rowMadEsc
+
+
--- lib/DHont.R
+++ lib/DHont.R
@@ -1,5 +1,5 @@
-library("dplyr", lib.loc="~/R/x86_64-redhat-linux-gnu-library/3.2")
-
+library("dplyr")
+library("reshape2")
 
 dHondt <- function (parties, votes, seats,threshold=0)
 { if (threshold != 0)
@@ -15,3 +15,61 @@ dHondt <- function (parties, votes, seats,threshold=0)
   names(out) <- c("Parties", "d'Hondt", "Perc")
   return(out)
 }
+
+#From FICHEROS.doc coming from 04201105_MESA.zip
+# “Tipo de elección” (columnas 1-2) que serán idénticos y se ajustarán a los siguientes:
+# 01 = Referéndum
+# 02 = Congreso
+# 03 = Senado
+# 04 = Municipales, Partidos Judiciales y Diputaciones Provinciales
+# 05 = Autonómicas
+# 06 = Cabildos Insulares
+# 07 = Parlamento Europeo
+# 15 = Juntas Generales.
+
+ElectionThreshold <- function(eleType)
+{
+  if (eleType == 4)
+  {
+    return(0.05)
+  } else if (eleType == 2)
+  {
+    return(0.03)
+  } else
+  {
+    return(0)
+  }
+}
+
+AllMunic <- function(datosEscrut)
+{
+  aux <- unique(datosEscrut$municData[,c(6,7)])
+
+  return(aux[order(aux$CodProv,aux$CodMun),])
+}
+
+ProcessMesa <- function(datosEscrut,prov,mun)
+{
+  AuxDatosMesa <- datosEscrut$mesaData
+  AuxResultMesa <- datosEscrut$mesaResult
+
+  DatosMesa <-
+    AuxDatosMesa[AuxDatosMesa$CodProv == prov &
+                   AuxDatosMesa$CodMun == mun,]
+  ResultMesa <- AuxResultMesa[AuxResultMesa$CodProv == prov &
+                                AuxResultMesa$CodMun == mun,]
+
+  ResultMesaAcr <-
+    merge(ResultMesa,datosEscrut$candListaData[,c(1:5)])
+  ResultMesaEscr <-
+    dcast(
+      data = ResultMesaAcr,formula = ElType + ElYear + ElMonth + Round +
+        CodProv + CodMun + MunDistrict + CodSeccion + CodMesa ~ candAcronym,
+      value.var = "CandVotos"
+    )
+  namesCands <- setdiff(names(ResultMesaEscr),names(ResultMesaAcr))
+  DatosMesaEscr <- merge(DatosMesa,ResultMesaEscr)
+  VotosCands <- DatosMesaEscr[,namesCands]
+
+}
+
